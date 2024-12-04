7dd57a002709cbcb3772faa020cbdb6cf776213a
Nguyễn Tạ Huy Hoàng <85854007+NguyenTaHuyHoang@users.noreply.github.com>
2024-12-04 15:37:25 +0700

    add foreignKey in EFCore and add ImageUrl Column

--- Bulky.Models/Product.cs
+++ Bulky.Models/Product.cs
@@ -5,6 +5,7 @@ using System.ComponentModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Bulky.Models
 {
@@ -46,5 +47,12 @@ namespace Bulky.Models
         [Range(1, 1000)]
         public double Price100 { get; set; }
 
+
+        // Khóa ngoại đến bảng Category
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+
+        public string ImageUrl { get; set; }
     }
 }
